320fc57339fed1126f48d303f0e6db2f9b72fce3
Auquier Cyril <c.auquier@student.helmo.be>
2024-01-02 21:55:21 +0100

    updating some stuff

--- app/barcodeScanner.tsx
+++ app/barcodeScanner.tsx
@@ -2,18 +2,16 @@
 
 import { useState, useEffect } from "react";
 import { useZxing } from "react-zxing";
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 export const BarcodeScanner = () => {
   const [result, setResult] = useState("");
   const [product, setProduct] = useState("")
+  console.log(process.env.NEXT_PUBLIC_API_KEY);
   const base_url = 'https://api.barcodelookup.com/v3/products'
   const { ref } = useZxing({
     async onDecodeResult(result) {
       setResult(result.getText());
-      await fetch(base_url + '?barcode=' + result.getText() + '&key=' + process.env.API_KEY)
+      await fetch(base_url + '?barcode=' + result.getText() + '&key=' + process.env.NEXT_PUBLIC_API_KEY)
         .then(result => console.log(result))
         .catch(err => console.log(err));
     },
