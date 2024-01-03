5db142bce9ef762110b3a17d7dd9d5ffe031136c
Auquier Cyril <c.auquier@student.helmo.be>
2024-01-03 11:50:07 +0100

    add some listening

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios')
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+
+app.get("/product/:barCode", async (req, res, next) => {
+    try {
+        const result = await axios.get('https://api.upcitemdb.com/prod/trial/lookup?upc=' + req.params.barCode);
+        const data = result.data;
+        if (data.items.length === 0) {
+            res.status(404).json({ error: "No product found" });
+        }
+        res.status(500).json(data.items[0]);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
